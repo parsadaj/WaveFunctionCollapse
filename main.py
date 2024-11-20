f8d1ca737fbb0611a87e4b3476c12f4f38fa7639
Parsa <53321170+parsadaj@users.noreply.github.com>
2024-11-20 15:50:06 +0330

    new data added

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from WFC import WaveFunctionCollapse
 from utils import save_state
 from functions import height_to_slopes, slopes_to_height
 
-
+import os
 # reading data
-sample_data_path = "data/N26E057.hgt"
+# sample_data_path = "data/N26E057.hgt"
+sample_data_path = "data/N27E060.hgt"
 
 
 scale_factor = 1.0 / 8
@@ -50,12 +51,17 @@ slopes = np.concatenate([grad_x[:-1, ..., np.newaxis], grad_y[..., :-1, np.newax
 wfc_terrain = WaveFunctionCollapse(slopes, (2,2,2), (20,20,2), remove_low_freq=False, low_freq=1)
 wfc_terrain.match_patterns()
 
-save_state(wfc_terrain, './results/wave_function_collapse_state.pkl')
+dirname = os.path.splitext(os.path.basename(sample_data_path))[0]
+os.makedirs(f"./results/{dirname}", exist_ok=True)
+save_state(wfc_terrain, f'./results/{dirname}/wfc_state.pkl')
 # save_state(wfc_terrain.pattern_frequencies, 'wfc_patterns.pkl')
 # save_state(wfc_terrain.adjacency_rules, 'wfc_adjacency.pkl')
 # save_state(wfc_terrain.pattern_to_number, 'wfc_pattern2num.pkl')
 
 # running WFC
 n_out = 0
-output_image = wfc_terrain.run()
-save_state(output_image, './results/wfc_out.pkl')
+
+while n_out < 2:
+    output_image = wfc_terrain.run()
+    save_state(output_image, f'./results/{dirname}/wfc_out.pkl')
+    n_out += 1
